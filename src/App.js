0c4336fa0f52969c0366b48161a1c7037a0f1694
Boluwatife <omotayodaniel10@gmail.com>
2022-07-22 20:30:28 +0100

    brb lol

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Quiz from "./components/Quiz";
 
 export default function App() {
-  const API_URL =
-    "https://opentdb.com/api.php?amount=5&category=18&difficulty=medium&type=multiple";
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    const getItems = async () => {
-      try {
-        const response = await fetch(API_URL);
-        const data = await response.json();
-        const result = data.results;
-        const questions = Object.values(result);
-        console.log(questions);
-        setItems(questions);
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    getItems();
-  }, []);
+  const [questions, setQuestions] = useState([]);
 
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="Quiz">
-          <Route index element={<Quiz items={items} setItems={setItems} />} />
+          <Route
+            index
+            element={<Quiz questions={questions} setQuestions={setQuestions} />}
+          />
         </Route>
       </Routes>
     </div>
